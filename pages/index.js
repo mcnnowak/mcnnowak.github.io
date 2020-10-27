a92ff6774c32dc6648605591aa1e297dbece6163
Michael Nowak <mcnnowak@gmail.com>
2020-10-27 15:39:14 -0700

    feat(*): Add netlify support
    * Following tutorial
    * Added netlify support

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          What is <a href="https://nextjs.org">Next.js?</a>
         </h1>
 
         <p className={styles.description}>
@@ -20,10 +21,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/posts/post">
+            <a className={styles.card}>
+              <h3>First post &rarr;</h3>
+              <p>The first post that I wrote on this Next.js blog.</p>
+            </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
