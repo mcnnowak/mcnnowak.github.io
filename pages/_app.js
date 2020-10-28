951bb819f7c90784b6a2a80edffaf801a9b88d55
Michael Nowak <mcnnowak@gmail.com>
2020-10-28 13:30:58 -0700

    feat(*): Configure express for TinaCMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '../styles/globals.css'
 import {TinaProvider, TinaCMS, useCMS } from 'tinacms'
+import { GitClient, GitMediaStore } from '@tinacms/git-client'
 
 function App({ Component, pageProps }) {
+  const client = new GitClient('/___tina')
   const cms = new TinaCMS({
-    sidebar: true
+    apis: {
+      git: client,
+    },
+    media: new GitMediaStore(client),
+    sidebar: {
+      position: 'overlay'
+    }
   })
+  cms.registerApi('git', client)
+  if(process.env.NODE_ENV === 'development') { cms.enable() }
+  else { cms.disable() }
 
   return <TinaProvider cms={cms}>
       <Component {...pageProps} />
