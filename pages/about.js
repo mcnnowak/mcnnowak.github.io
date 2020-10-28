93ed64face4a5f084273dc6103857a96b343f3f9
Michael Nowak <mcnnowak@gmail.com>
2020-10-28 13:31:25 -0700

    feat(*): Add about page stub, blog stub, remove TinaButton CSS

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function About() {
+  return <>
+    <Layout>
+      <Head>
+        <title>About</title>
+      </Head>
+      Under construction!
+    </Layout>
+  </>
+}
